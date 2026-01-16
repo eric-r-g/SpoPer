f481032dcfbe792b032b18986983075030a9795a
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2026-01-16 19:00:07 -0300

    Adicionando quase todas as restrições

--- scripts/criação do banco/Script_SpotPer.sql
+++ scripts/criação do banco/Script_SpotPer.sql
@@ -6,39 +6,21 @@ CREATE TABLE gravadora (
 	endr_homepage varchar(256) NOT NULL
 );
 
--- "Um álbum, com faixas de músicas do período barroco, só pode ser inserido no
--- banco de dados, caso o tipo de gravação seja DDD."
--- "Um álbum não pode ter mais que 64 faixas (músicas)."
--- "No caso de remoção de um álbum do banco de dados, todas as suas faixas
--- devem ser removidas. Lembre-se que faixas podem apresentar, por sua vez,
--- outros relacionamentos"
--- "O preço de compra de um álbum não dever ser superior a três vezes a média
--- do preço de compra de álbuns, com todas as faixas com tipo de gravação
--- DDD."
--- Tem que ver se tem como colocar um check pra quando o tipo do disco for cd, o tipo de gravação
--- ser add ou ddd, e quando não for, ser nulo.
 CREATE TABLE album (
 	cod int PRIMARY KEY,
     nome varchar(64) NOT NULL,
 	descricao varchar(256) NOT NULL,
 	gravadora int REFERENCES gravadora (cod),
-	preco_cmpr real NOT NULL, -- Dar uma pesquisada se esse é o melhor tipo mesmo
+	preco_cmpr real NOT NULL,
 	data_cmpr date NOT NULL,
 	data_grav date NOT NULL,
 	
 	CHECK(data_grav > date '01-01-2000')
-
-	-- Segunda restrição
-	--SELECT count(*) 
-	--FROM faixa f, meio_fisico m, album a
-	--WHERE m.album=a.cod and f.disco=m.cod
-	--GROUP BY a.cod
-	--CHECK(select count(*) from faixa f, meio_fisico m where m.)
 );
 
 CREATE TABLE meio_fisico (
 	cod int PRIMARY KEY,
-	album int REFERENCES album(cod),
+	album int REFERENCES album(cod) ON DELETE CASCADE,
 	tipo varchar(8) NOT NULL
 );
 
@@ -74,7 +56,7 @@ CREATE TABLE faixa (
 	cod int PRIMARY KEY,
     nome varchar(64) NOT NULL,
 	pos_album int NOT NULL,
-	disco int REFERENCES meio_fisico (cod), -- Talvez mudar o nome dessa coluna
+	disco int REFERENCES meio_fisico (cod) ON DELETE CASCADE, -- Talvez mudar o nome dessa coluna
 	descricao varchar(256) NOT NULL,
 	tipo_grav varchar(3),
 	tipo_comp int REFERENCES tipo_composicao (cod)
@@ -89,12 +71,12 @@ CREATE TABLE faixa (
 CREATE TABLE composicao (
 	cod int PRIMARY KEY,
 	compositor int REFERENCES compositor (cod),
-	faixa int REFERENCES faixa (cod)
+	faixa int REFERENCES faixa (cod) ON DELETE CASCADE
 );
 CREATE TABLE interpretacao (
 	cod int PRIMARY KEY,
 	interprete int REFERENCES interprete (cod),
-	faixa int REFERENCES faixa (cod)
+	faixa int REFERENCES faixa (cod) ON DELETE CASCADE
 );
 
 CREATE TABLE playlist (
@@ -107,7 +89,7 @@ CREATE TABLE playlist (
 CREATE TABLE playlist_contem (
 	cod int PRIMARY KEY,
 	playlist int REFERENCES playlist (cod),
-	faixa int REFERENCES faixa (cod),
+	faixa int REFERENCES faixa (cod) ON DELETE CASCADE,
 	ult_data_tocada date,
 	numero_tocada int NOT NULL DEFAULT 0
 );
--- scripts/gatilhos/check_barroco_ddd.sql
+++ scripts/gatilhos/check_barroco_ddd.sql
@@ -6,15 +6,16 @@
 	Como esse trigger tá em album, 
 	se o valor de tipo de gravação ou o periodo do compositor mudar,
 	o trigger não vai ativar e daí dá pra quebrar a restrição.
+
+	Uma forma bem mais garantida de fz isso é adicionar esse trigger em faixa,
+	se tipo_comp da faixa é barroca, garantir que o disco dela é DDD.
+	O problema é que isso não é exatamente o que ele tá falando no item.
 */
 
 CREATE OR REPLACE FUNCTION check_barroco_ddd() RETURNS TRIGGER AS $check_barroco_ddd$
 DECLARE
 	is_barroca BOOLEAN = false;
 BEGIN
-	-- Variáveis importantes:
-	-- NEW -> Tupla que está sendo adicionada
-
 	-- Checar se tem faixa barroca
 
 	-- Se sim checar tipo_grav, senão retorna NEW
